0bc11b537f6f1b85163156cb150dc9a363105ab8
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2025-11-03 15:19:15 -0500

    Conexión a la DB

--- CocosBurger/settings.py
+++ CocosBurger/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'CocosBurger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'DBCocosB',
+        'USER': 'django_user',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
     }
 }
 
