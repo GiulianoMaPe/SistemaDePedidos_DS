a8b038dc81263395253218c73f9ccf6c287f1289
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2025-12-08 01:39:41 -0500

    NuevaBD

--- productos/models.py
+++ productos/models.py
@@ -16,6 +16,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     categoria = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
     insumos = models.ManyToManyField(Insumo, blank=True, related_name='productos')
 
     def __str__(self):
